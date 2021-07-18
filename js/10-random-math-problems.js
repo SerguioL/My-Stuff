cf2ddac7f39d3005573c2a1dedfc49fa7be01158
SerguioL <serguio.l.landa@gmail.com>
2021-07-18 18:44:25 -0500

    modify code to fit inside one function

--- js/10-random-math-problems.js
+++ js/10-random-math-problems.js
@@ -1,12 +1,15 @@
-let random1 = Math.floor((Math.random()) * 101 );
-
-let random2 = Math.floor((Math.random()) * 101 );
-
-console.log(random1,random2);
-
-let questionOne = prompt("What is " + random1 + " times " + random2);
+// let random1 = Math.floor((Math.random()) * 101 );
+//
+// let random2 = Math.floor((Math.random()) * 101 );
+//
+// console.log(random1,random2);
+//
+// let questionOne = prompt("What is " + random1 + " times " + random2);
 
 function answerOne(){
+    let random1 = Math.floor((Math.random()) * 101 );
+    let random2 = Math.floor((Math.random()) * 101 );
+    let questionOne = prompt("What is " + random1 + " times " + random2);
     let answer = random1 * random2;
     if(answer === questionOne){
         return alert("Good Job!");
