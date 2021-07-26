4ff8f187ef35d49a90ba6a405ab124aa7fb6895e
SerguioL <serguio.l.landa@gmail.com>
2021-07-25 19:22:43 -0500

    added prompt and different mode difficultys easy and hard

--- js/10-random-math-problems.js
+++ js/10-random-math-problems.js
@@ -6,15 +6,35 @@
 //
 // let questionOne = prompt("What is " + random1 + " times " + random2);
 
-function answerOne(){
-    let random1 = Math.floor((Math.random()) * 101 );
-    let random2 = Math.floor((Math.random()) * 101 );
-    let questionOne = prompt("What is " + random1 + " times " + random2);
-    let answer = random1 * random2;
-    if(answer === questionOne){
-        return alert("Good Job!");
-    } else {
-        return alert("Sorry, the correct answer was " + answer);
+let modeDifficulty = prompt("for easy mode type EASY for hard mode type HARD")
+
+if(modeDifficulty === "HARD") {
+    function hardQuestion(input) {
+        let random1 = Math.floor((Math.random()) * 101);
+        let random2 = Math.floor((Math.random()) * 101);
+        prompt("What is " + random1 + " times " + random2);
+        let answer = random1 * random2;
+        if (answer === parseFloat(input)) {
+            return alert("Good Job!");
+        } else {
+            return alert("Sorry, the correct answer was " + answer);
+        }
     }
+    console.log(hardQuestion())
 }
- console.log(answerOne());
+
+if(modeDifficulty === "EASY") {
+    function easyQuestion(input) {
+        let random1 = Math.floor((Math.random()) * 11);
+        let random2 = Math.floor((Math.random()) * 11);
+        prompt("What is " + random1 + " times " + random2);
+        let answer = random1 * random2;
+        if (parseFloat(input) == answer) {
+            return alert("Good Job!");
+        } else {
+            return alert("Sorry, the correct answer was " + answer);
+        }
+    }
+
+    console.log(easyQuestion())
+}
